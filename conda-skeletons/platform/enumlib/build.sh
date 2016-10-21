c3bffbf635401e0f50935e98012ec5ac5fdb3574
__sp__ <ongsp@eng.ucsd.edu>
2016-10-21 07:24:20 -0700

    MUch better build script fpr enumlib.

---
+++ conda-skeletons/platform/enumlib/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BINARY=enum
+BINARY_HOME=$PREFIX/bin
+
+git clone https://github.com/msg-byu/enumlib.git
+git clone https://github.com/msg-byu/symlib.git
+
+#unzip symlib
+#unzip enumlib
+
+cd $SRC_DIR/symlib/src
+
+export F90=gfortran
+make
+
+cd $SRC_DIR/enumlib/src
+
+make
+make enum.x
+make makestr.x
+
+cp enum.x $BINARY_HOME
+cp makestr.x $BINARY_HOME
