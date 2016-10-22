8ae5b090615310a8be0b45b62f2702f0561f352c
__sp__ <ongsp@eng.ucsd.edu>
2016-10-21 19:33:51 -0700

    Add bld.bat.

---
+++ conda-skeletons/platform/enumlib/bld.bat
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BINARY=enum
+BINARY_HOME=%PREFIX%\bin
+
+git clone https://github.com/msg-byu/enumlib.git
+git clone https://github.com/msg-byu/symlib.git
+
+cd %SRC_DIR%\symlib\src
+
+F90=gfortran
+make
+
+cd %SRC_DIR%\enumlib\src
+
+make
+make enum.x
+make makestr.x
+
+cp enum.x %BINARY_HOME%
+cp makestr.x %BINARY_HOME%
