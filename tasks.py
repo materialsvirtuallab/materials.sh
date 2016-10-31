e317f991a88a14b0e123e990d6bb1395d08507cc
__sp__ <ongsp@eng.ucsd.edu>
2016-10-31 12:22:05 -0700

    Add skeletons for pg-db and diffusion.

--- tasks.py
+++ tasks.py
@@ -3,11 +3,12 @@
 
 import glob
 import os
-import yaml
+import shutil
 
 from invoke import task
 
 from monty.os import cd
+from monty.serialization import loadfn, dumpfn
 
 __author__ = "Shyue Ping Ong"
 __email__ = "ongsp@ucsd.edu"
@@ -15,11 +16,33 @@ __date__ = "Sep 1, 2014"
 
 
 @task
-def build_conda(ctx, pkg, nopy27=False):
-    print(pkg)
+def update_pypi(ctx, pkg):
     with cd(os.path.join("conda-skeletons")):
-        with open(os.path.join(pkg, "meta.yaml"), "rt") as f:
-            d = yaml.load(f)
+        meta = os.path.join(pkg, "meta.yaml")
+        noarch = False
+        if os.path.exists(meta):
+            d = loadfn(meta)
+            noarch = d.get("build", {}).get("noarch_python", False)
+            shutil.rmtree(pkg)
+        if not noarch:
+            if pkg == "pymatgen":
+                ctx.run("conda skeleton pypi --setup-options='--single-version-externally-managed --record=record.txt' %s" % pkg)
+            else:
+                ctx.run("conda skeleton pypi %s" % pkg)
+        else:
+            ctx.run("conda skeleton pypi --noarch-python %s" % pkg)
+        if pkg == "pymatgen":
+            d = loadfn(meta)
+            d["requirements"]["build"].append("enum34")
+            d["requirements"]["run"].append("enum34")
+            dumpfn(d, meta, default_flow_style=False)
+            ctx.run('sed -i "" "s/enum34/enum34 #[py27]/g" %s' % meta)
+
+
+@task
+def build_conda(ctx, pkg, nopy27=False):
+    with cd(os.path.join("conda-skeletons")):
+        d = loadfn(os.path.join(pkg, "meta.yaml"))
         noarch = d.get("build", {}).get("noarch_python", False)
         if noarch:
             ctx.run("conda build %s" % pkg)
