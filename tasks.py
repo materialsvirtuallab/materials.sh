e48081447e65473b2bae78a2c91fb9f66efd04fa
__sp__ <ongsp@eng.ucsd.edu>
2016-11-28 12:15:49 -0800

    Auto upload.

--- tasks.py
+++ tasks.py
@@ -47,30 +47,30 @@ def build_conda(ctx, pkg, nopy27=False):
         d = loadfn(os.path.join(pkg, "meta.yaml"))
         noarch = d.get("build", {}).get("noarch_python", False)
         if noarch:
-            ctx.run("conda build %s" % pkg)
-            fnames = glob.glob(os.path.join(os.path.expanduser("~"),
-                                            "miniconda3", "conda-bld", "noarch",
-                                            "%s-*py*.tar.bz2" % pkg))
-            latest = sorted(fnames)[-1]
-            ctx.run("anaconda upload --force --user matsci %s" % latest)
+            ctx.run("conda build --user matsci %s" % pkg)
+            # fnames = glob.glob(os.path.join(os.path.expanduser("~"),
+            #                                 "miniconda3", "conda-bld", "noarch",
+            #                                 "%s-*py*.tar.bz2" % pkg))
+            # latest = sorted(fnames)[-1]
+            # ctx.run("anaconda upload --force --user matsci %s" % latest)
         else:
             # Py35 versions
-            ctx.run("conda build %s" % pkg)
-            fnames = glob.glob(os.path.join(os.path.expanduser("~"),
-                                            "miniconda3", "conda-bld", "*",
-                                            "%s-*py35*.tar.bz2" % pkg))
-            latest = sorted(fnames)[-1]
-            ctx.run("anaconda upload --force --user matsci %s" % latest)
+            ctx.run("conda build --user matsci %s" % pkg)
+            # fnames = glob.glob(os.path.join(os.path.expanduser("~"),
+            #                                 "miniconda3", "conda-bld", "*",
+            #                                 "%s-*py35*.tar.bz2" % pkg))
+            # latest = sorted(fnames)[-1]
+            # ctx.run("anaconda upload --force --user matsci %s" % latest)
 
             if not nopy27:
                 # Py27 versions
-                ctx.run("conda build --python 2.7 %s" % pkg)
-                fnames = glob.glob(os.path.join(os.path.expanduser("~"),
-                                                "miniconda3", "conda-bld", "*",
-                                                "%s-*py27*.tar.bz2" % pkg))
+                ctx.run("conda build --user matsci --python 2.7 %s" % pkg)
+                # fnames = glob.glob(os.path.join(os.path.expanduser("~"),
+                #                                 "miniconda3", "conda-bld", "*",
+                #                                 "%s-*py27*.tar.bz2" % pkg))
 
-                latest = sorted(fnames)[-1]
-                ctx.run("anaconda upload --force --user matsci %s" % latest)
+                # latest = sorted(fnames)[-1]
+                # ctx.run("anaconda upload --force --user matsci %s" % latest)
 
 @task
 def build_all(ctx, nopy27=False):
