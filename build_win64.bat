4e370b1810560cd2c33e5265584b218154413f20
__sp__ <ongsp@eng.ucsd.edu>
2016-11-29 17:46:13 -0500

    Fix appveyor.

--- build_win64.bat
+++ build_win64.bat
@@ -2,6 +2,6 @@ cd conda-skeletons
 conda install conda-build anaconda-client
 conda config --add channels matsci
 conda config --set anaconda_upload yes
-anaconda login --hostname travis-mavrl-win64-py35 --username %ANACONDA_USER% --password %ANACONDA_PASSWORD%
+anaconda login --hostname appveyor-mavrl-win64-py35 --username %ANACONDA_USER% --password %ANACONDA_PASSWORD%
 FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO conda build --user matsci %%A
 cd ..
