dca2d87a5f0070a20bac3b2a658ba3ca1becb5cd
__sp__ <ongsp@eng.ucsd.edu>
2016-11-29 15:42:48 -0500

    Fix user.

--- build_win64.bat
+++ build_win64.bat
@@ -3,5 +3,5 @@ conda install conda-build anaconda-client
 conda config --add channels matsci
 conda config --set anaconda_upload yes
 anaconda login --hostname travis-mavrl-win64-py35 --username %ANACONDA_USER% --password %ANACONDA_PASSWORD%
-FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO conda build %%A
+FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO conda build --user matsci %%A
 cd ..
