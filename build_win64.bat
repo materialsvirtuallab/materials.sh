6d255e4fca8e477c5ba915bd68d3b07405a74288
__sp__ <ongsp@eng.ucsd.edu>
2016-11-29 15:39:58 -0500

    Proper anaconda upload.

--- build_win64.bat
+++ build_win64.bat
@@ -1,4 +1,7 @@
 cd conda-skeletons
+conda install conda-build anaconda-client
+conda config --add channels matsci
+conda config --set anaconda_upload yes
+anaconda login --hostname travis-mavrl-win64-py35 --username %ANACONDA_USER% --password %ANACONDA_PASSWORD%
 FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO conda build %%A
-FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO anaconda upload %HOMEPATH%\Miniconda3\win-64\%%A-*py35*.tar.bz2 -u matsci
 cd ..
