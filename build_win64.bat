bd764f16bb37d9ffc437f090188ebb7f15104f8f
__sp__ <ongsp@eng.ucsd.edu>
2016-11-29 19:58:10 -0500

    Increment numpy version.

--- build_win64.bat
+++ build_win64.bat
@@ -2,5 +2,5 @@ cd conda-skeletons
 conda install conda-build anaconda-client
 conda config --add channels matsci
 conda config --set anaconda_upload yes
-FOR %%A IN (latexcodec tabulate monty pybtex palettable spglib pydispatcher pymatgen) DO conda build --user matsci %%A
+FOR %%A IN (latexcodec pybtex spglib pymatgen) DO conda build --user matsci %%A
 cd ..
