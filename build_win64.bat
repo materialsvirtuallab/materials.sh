5ea555019a3ffeeed97342a2a7ff1576c321a179
__sp__ <ongsp@eng.ucsd.edu>
2016-12-03 06:19:34 -0800

    ADd pymatgen-db to windwos build.

--- build_win64.bat
+++ build_win64.bat
@@ -2,5 +2,5 @@ cd conda-skeletons
 conda install conda-build anaconda-client
 conda config --add channels matsci
 conda config --set anaconda_upload yes
-FOR %%A IN (latexcodec pybtex spglib pyhull pymatgen seekpath) DO conda build --user matsci %%A
+FOR %%A IN (latexcodec pybtex spglib pyhull pymatgen pymatgen-db seekpath) DO conda build --user matsci %%A
 cd ..
