9b62acd12fa259e67fe7dea65bcc4f56a54557b2
__sp__ <ongsp@eng.ucsd.edu>
2016-12-05 09:39:43 -0800

    Add skip existing to appveyor.

--- build_win64.bat
+++ build_win64.bat
@@ -2,5 +2,5 @@ cd conda-skeletons
 conda install conda-build anaconda-client
 conda config --add channels matsci
 conda config --set anaconda_upload yes
-FOR %%A IN (ase latexcodec pybtex spglib pyhull pymatgen pymatgen-db seekpath) DO conda build --user matsci %%A
+FOR %%A IN (ase latexcodec pybtex spglib pyhull pymatgen pymatgen-db seekpath) DO conda build  --skip-existing --user matsci %%A
 cd ..
