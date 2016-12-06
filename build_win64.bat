890656f5da73778a3be0e60dc99dc9503a4cb93e
__sp__ <ongsp@eng.ucsd.edu>
2016-12-06 02:24:08 -0800

    Try removing skip.

--- build_win64.bat
+++ build_win64.bat
@@ -2,6 +2,6 @@ cd conda-skeletons
 conda install conda-build anaconda-client
 conda config --add channels matsci
 conda config --set anaconda_upload yes
-FOR %%A IN (ase latexcodec pybtex spglib pyhull pymatgen pymatgen-db seekpath) DO conda build  --skip-existing --user matsci %%A
-FOR %%A IN (ase latexcodec pybtex spglib pyhull pymatgen pymatgen-db seekpath) DO conda build  --skip-existing --user matsci --python 2.7 %%A
+FOR %%A IN (ase latexcodec pybtex spglib pymatgen pymatgen-db seekpath) DO conda build  --skip-existing --user matsci %%A
+FOR %%A IN (ase latexcodec pybtex spglib pymatgen pymatgen-db seekpath) DO conda build  --skip-existing --user matsci --python 2.7 %%A
 cd ..
