3b73612479bdbb3d63fa58b6134714ff31e88c08
__sp__ <ongsp@eng.ucsd.edu>
2016-12-08 15:20:15 -0800

    Remove noarch.

--- build_win64.bat
+++ build_win64.bat
@@ -2,6 +2,5 @@ cd conda-skeletons
 conda install conda-build anaconda-client
 conda config --add channels matsci
 conda config --set anaconda_upload yes
-FOR %%A IN (ase latexcodec pybtex spglib pymatgen pymatgen-db seekpath) DO conda build  --skip-existing --user matsci %%A
-FOR %%A IN (ase latexcodec pybtex spglib pymatgen pymatgen-db seekpath) DO conda build  --skip-existing --user matsci --python 2.7 %%A
+FOR /D %%G in ("*") DO conda build  --skip-existing --user matsci %%G
 cd ..
