f3c6b4348cc45db4e76feac97b69466d8845068d
__sp__ <ongsp@eng.ucsd.edu>
2016-12-08 15:09:05 -0800

    Add tqdm.

--- build_win64.bat
+++ build_win64.bat
@@ -3,4 +3,5 @@ conda install conda-build anaconda-client
 conda config --add channels matsci
 conda config --set anaconda_upload yes
 FOR %%A IN (ase latexcodec pybtex spglib pymatgen pymatgen-db seekpath) DO conda build  --skip-existing --user matsci %%A
+FOR %%A IN (ase latexcodec pybtex spglib pymatgen pymatgen-db seekpath) DO conda build  --skip-existing --user matsci --python 2.7 %%A
 cd ..
