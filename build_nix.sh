33d41237fe478a595ca74744eb0c6bea358b6630
__sp__ <ongsp@eng.ucsd.edu>
2017-01-16 15:48:08 -0800

    Fix build nix.

--- build_nix.sh
+++ build_nix.sh
@@ -1,5 +1,8 @@
 set -e
 cd conda-skeletons
+conda config --add channels matsci
+conda install tabulate --yes
+conda update --all --yes
 for pkg in `ls -d *`
 do
     conda build --skip-existing --user matsci $pkg
