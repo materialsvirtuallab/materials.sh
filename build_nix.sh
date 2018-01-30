10619d5805af7b70f28b0e17f54eb360fc2ba6ad
Shyue Ping Ong <shyuep@gmail.com>
2018-01-30 07:15:12 -0800

    Build py27 versions within the same build_nix.

--- build_nix.sh
+++ build_nix.sh
@@ -9,3 +9,8 @@ do
         conda build --skip-existing --user matsci $pkg
     fi
 done
+do
+    if [ "$pkg" != "atomate" ]; then
+        conda build --skip-existing --user matsci --python 2.7 $pkg
+    fi
+done
