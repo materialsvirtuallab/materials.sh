05db2af9ebd2591d78fe4154c42d030a8261c34e
__sp__ <ongsp@ucsd.edu>
2018-01-30 08:57:48 -0800

    Remove conda update all.

--- build_nix.sh
+++ build_nix.sh
@@ -2,7 +2,7 @@ set -e
 cd conda-skeletons
 conda build --skip-existing --user matsci tabulate
 conda build --skip-existing --user matsci monty
-conda update --all --yes
+# conda update --all --yes
 for pkg in `ls -d *`
 do
     if [ "$pkg" != "atomate" ]; then
