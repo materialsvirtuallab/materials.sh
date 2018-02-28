4d1c3fb789089bb4476761a88082f4fcd39037df
__sp__ <shyuep@gmail.com>
2018-02-27 19:38:04 -0800

    Cleanup.

--- install_conda.sh
+++ install_conda.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    if [[ "$TOXENV" == "py27" ]]; then
-        curl -o miniconda.sh https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh;
-    else
-        curl -o miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh;
-    fi
-else
-    if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-        wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
-    else
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-    fi
-fi
-
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 hash -r
@@ -24,13 +11,7 @@ conda info -a
 conda install conda-build anaconda-client
 conda config --add channels matsci
 conda config --add channels conda-forge
-if [[ "$TOXENV" == "py36" ]]; then
-    conda install python==3.6 --yes
-fi
+conda install python==3.6 --yes
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    conda config --set anaconda_upload yes;
-    python login.py;
-else
-    conda config --set anaconda_upload no;
-fi
+conda config --set anaconda_upload yes;
+python login.py;
