0ae163aa3a2e6d03c60ad1a332c96cc055986332
Shyue Ping Ong <shyuep@gmail.com>
2018-04-25 14:27:09 -0700

    Explicitly build ruamel.

--- build_win64.bat
+++ build_win64.bat
@@ -4,6 +4,6 @@ conda config --add channels matsci
 conda config --set anaconda_upload yes
 conda build --skip-existing --user matsci tabulate
 conda build --skip-existing --user matsci monty
+conda build --user matsci ruamel.yaml
 FOR /D %%G in ("*") DO conda build  --skip-existing --user matsci %%G
-FOR /D %%G in ("*.*") DO conda build  --skip-existing --user matsci %%G
 cd ..
