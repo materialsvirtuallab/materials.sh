95c71d4588dbd847cf7d5a257f2d1a4c1c5ee13e
__sp__ <shyuep@gmail.com>
2019-01-28 12:05:38 -0800

    Fix windows.

--- build_win64.bat
+++ build_win64.bat
@@ -1,6 +1,6 @@
 cd conda-skeletons
-conda install conda-build anaconda-client
 conda install -n base conda==4.3.34 --yes
+conda install anaconda-client
 conda config --add channels matsci
 conda config --set anaconda_upload yes
 conda build --skip-existing --user matsci tabulate
